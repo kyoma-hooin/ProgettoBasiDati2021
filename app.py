673eaae87422de5299507b265658deaf1d637f6d
Darth Simous <azkaburr2000@gmail.com>
2021-04-22 17:38:52 +0200

    hello world

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+
+app = Flask(__name__)
+engine = create_engine('sqlite:///C:\\Università\\Secondo Anno\\Basi di Dati\\Modulo 2\\Progetto\\database.db')
+Base = declarative_base()
+
+@app.route('/')
+def hello_world():
+    Base.metadata.create_all(engine)
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
