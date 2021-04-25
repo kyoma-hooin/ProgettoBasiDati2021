583b6aa571acd1b0d23828cfe62adc25c8f5e61d
Darth Simous <azkaburr2000@gmail.com>
2021-04-25 13:20:41 +0200

    postgres db

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from models import Base
 
 app = Flask(__name__)
-engine = create_engine('sqlite:///C:\\Università\\Secondo Anno\\Basi di Dati\\Modulo 2\\Progetto\\database.db')
-Base = declarative_base()
+DATABASE_URI = 'postgresql+psycopg2://postgres:progetto2021@localhost:5432/palestre'
+engine = create_engine(DATABASE_URI)  # 'sqlite:///C:\\Università\\Secondo Anno\\Basi di Dati\\Modulo
+# 2\\Progetto\\database.db'
+
 
 @app.route('/')
 def hello_world():
     Base.metadata.create_all(engine)
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run()
