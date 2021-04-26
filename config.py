6e4b0780c1904f99dbc0a8a95f70db042c995eef
Leonardo <leo.dechi99@gmail.com>
2021-04-26 13:08:23 +0200

    test

--- config.py
+++
@@ -1,3 +0,0 @@
-
-# Scheme: "postgresql+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-DATABASE_URI = 'insert your DATABASE_URI'
--- user.py
+++ user.py
@@ -1,9 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URI
 
 Base = declarative_base()  # We need to inherit Base in order to register user with SQAlchemy
 engine = create_engine(DATABASE_URI)  # engine now gives SQAlchemy the power to create tables
+
+Session = sessionmaker(bind=engine)  # To make a session, we use the sessionmaker class with engine
+s = Session()
 
 
 class User(Base):
@@ -19,3 +23,9 @@ class User(Base):
 
 Base.metadata.create_all(engine)
 # Base.metadata.drop_all(engine)
+
+user = User(id=0, name='Leonardo', surname='De Checchi')
+
+s.add(user)
+s.commit()
+s.close()
